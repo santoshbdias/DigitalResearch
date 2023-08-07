f1f36d3e96e90076751ccb94f9f4aa305c145eb6
santoshbdias <santosdiasagro@gmail.com>
2023-08-06 23:31:35 -0300

    www.santoshbdias.com.br

--- R/analises_sentinel.R
+++ R/analises_sentinel.R
@@ -111,11 +111,11 @@ analises_sentinel <- function(kml, past, savall) {
 
     print("Saving PNG image")
 
-    png(paste0(cm_plot,'/NDVI_',dat,".png"),
+    base::png(paste0(cm_plot,'/NDVI_',dat,".png"),
         width=1500, height=1350, res=200)
-    plot(NDVI, main=paste0('NDVI',' | ',dat,' | ',nkml))
-    plot(area_shape, add=T)
-    dev.off()
+    base::plot(NDVI, main=paste0('NDVI',' | ',dat,' | ',nkml))
+    base::plot(area_shape, add=T)
+    base::dev.off()
 
     print("Extracting values from each polygon")
 
